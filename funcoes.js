0ea029b188beaf4cda77ffbc68b744592202e1b4
joaopedrobordin <bpj7@hotmail.com>
2021-03-31 21:03:02 -0300

    dia5

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+let valorEmDolarTexto = prompt("qual o valor em dolar");
+console.log( typeof valorEmDolarTexto);
+
+let valorEmDolarNumero = parseFloat(valorEmDolarTexto)
+console.log(typeof valorEmDolarNumero);
+// float = número decimal -> 12.99
+// int = número inteiro -> 12
+
+document.querySelector("container").innerHTML(valorEmDolarNumero);
+
+let valorEmReal = valorEmDolarNumero * 5.77;
+
+let valorEmRealFixado = valorEmReal.toFixed(2);
+
+document.querySelector("container").innerHTML(valorEmRealFixado);
